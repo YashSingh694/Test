b5fecb39ff1bce248af8471c781b2ee71f183b13
YashSingh694 <yashsingh694@gmail.com>
2017-05-12 00:44:26 +0530

    Update A.java

--- A.java
+++ A.java
@@ -4,8 +4,7 @@ class A
  public static void main(String []args)
  {
  System.out.println("This is my class");
-  System.out.println("This is my java class");
-  System.out.println("Extra");  
+  System.out.println("This is my java cla");
   System.out.println("This is git bash UI");
  
 }
