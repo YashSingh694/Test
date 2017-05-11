1211071198b7f1710bcfe00809291c5d826fb37c
YashSingh694 <yashsingh694@gmail.com>
2017-05-11 22:13:16 +0530

    Update A.java

--- A.java
+++ A.java
@@ -1,7 +1,11 @@
+#This is my progrm which I have written and checkin.
 class A
 {
  public static void main(String []args)
  {
  System.out.println("This is my class");
+  System.out.println("This is my java class");
+  System.out.println("This is git bash UI");
+ 
 }
 }
